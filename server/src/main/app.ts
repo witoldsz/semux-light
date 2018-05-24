6141f8e4ac6911f55a41d5163a8ab0db3382f2e8
Witold Szczerba <witoldsz.dev@gmail.com>
2018-05-24 22:58:59 +0200

    proxy server error handler

--- server/src/main/app.ts
+++ server/src/main/app.ts
@@ -12,9 +12,10 @@ async function main() {
   const proxy = httpProxy.createProxy({
     target: 'http://localhost:5171',
     auth: 'user:123456',
+    proxyTimeout: 5000,
   })
   const proxyMiddleware = (req, res, next) => {
-    proxy.web(req, res, undefined, console.error)
+    proxy.web(req, res, undefined, next)
   }
   app.get('/v2.1.0/info', proxyMiddleware)
   app.get('/v2.1.0/account', proxyMiddleware)
@@ -23,6 +24,13 @@ async function main() {
   app.get('/v2.1.0/delegates', proxyMiddleware)
   app.get('/v2.1.0/latest-block', proxyMiddleware)
   app.post('/v2.1.0/transaction/raw', proxyMiddleware)
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      success: false,
+      message: `proxy server error: ${err.message}`,
+    })
+  })
 
   const addr = await new Promise((resolve) => {
     const server = app.listen(3333, () => resolve(server.address()))

8cdf68f01d0fdc05211d2d9049ffa410eb960fbd
Witold Szczerba <witoldsz.dev@gmail.com>
2018-05-27 01:32:01 +0200

    API: handle unspecified errors better

--- front/src/main/lib/api.ts
+++ front/src/main/lib/api.ts
@@ -1,5 +1,9 @@
 export async function exec<T>(method: string, path: string): Promise<T> {
   const r = await fetch(path, { method })
-  const { success, message, result } = await r.json()
-  return success ? result : Promise.reject(new Error(message))
+  try {
+    const { success, message, result } = await r.json()
+    return success ? result : Promise.reject(new Error(message))
+  } catch (e) {
+    return Promise.reject(new Error(`Error: ${r.status} ${r.statusText}`))
+  }
 }
